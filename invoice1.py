989900de9b18a8f920881b75c3ac92450768f406
HarryKYS <110764639+HarryKYS@users.noreply.github.com>
2022-09-25 20:36:17 +0900

    invoice1

---
+++ invoice1.py
@@ -0,0 +1,19 @@
+import pyautogui
+# pyautogui.mouseInfo()
+import pyperclip
+import sys
+pyautogui.click(2199, 345, duration=1, button="left")
+pyautogui.sleep(1)
+con = pyautogui.confirm('계속 진행하시겠습니까?', buttons=['OK', 'Cancel'])
+
+if con == 'OK':
+    my_write = [1,2]
+    for invoice in my_write:
+        pyperclip.copy(invoice)
+        pyautogui.click(2199, 345, button="left")
+        pyautogui.hotkey("ctrl", "v")
+        pyautogui.press("enter")
+    my_write(invoice)
+   
+else:
+    sys.exit()
