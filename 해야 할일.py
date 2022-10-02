9fd4dc2e8a233e59f54bdda25d0236004eb0fbcb
HarryKYS <nathaniel3@naver.com>
2022-10-02 18:20:56 +0900

    bs4

--- 해야 할일.py
+++ 해야 할일.py
@@ -1,6 +1,7 @@
 # 1. mac moseinfo RGB Color 나오지 않는거 해결 
 # ㄴ 해결 후 답 적기 
 # 2.mac 창 tex sizs 작게 하는거 확인해보기 
+<<<<<<< Updated upstream
 
 
 # https://ohmyz.sh/
@@ -10,3 +11,5 @@
 # https://github.com/jaybk051183/powerlevel-10k
 
 # https://github.com/romkatv/powerlevel10k/blob/master/font.md
+=======
+>>>>>>> Stashed changes
